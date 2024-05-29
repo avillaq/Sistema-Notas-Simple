fbca50779c976e09129b9bcda0b6456388fbaf2a
avillaq <villafuertequispealex@gmail.com>
2024-05-28 23:19:48 -0500

    Añadimos el modelo alumno con sus campos : id_alumno, nombre_alumno, apellido_alumno

--- sistema/models.py
+++ sistema/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Alumno(models.Model):
+    id_alumno = models.AutoField(primary_key=True)
+    nombre_alumno = models.CharField(max_length=30)
+    apellido_alumno = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.nombre_alumno
