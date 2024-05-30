2dbc408bd8708e06f3d84e0bfd5b0ceb6f70e2da
avillaq <villafuertequispealex@gmail.com>
2024-05-29 21:20:11 -0500

    Se añade al path de urls las nuevas plantillas

--- sistema/urls.py
+++ sistema/urls.py
@@ -4,4 +4,8 @@ from sistema import views
 
 urlpatterns = [
     path('', view=views.index, name='index'),
+    path('/crear_alumno', view=views.crear_alumno, name='crear_alumno'),
+    path('/crear_curso', view=views.crear_curso, name='crear_curso'),
+    path('/asignar_notas', view=views.asignar_notas, name='asignar_notas'),
+
 ]
