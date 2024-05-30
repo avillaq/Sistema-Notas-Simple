a349528be4b85d44b4813e6e0d9678a5a092482a
avillaq <villafuertequispealex@gmail.com>
2024-05-29 21:23:47 -0500

    Creamos una vista para las nuevas plantillas

--- sistema/views.py
+++ sistema/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def crear_alumno(request):
+    return render(request, 'crearAlumno.html')
+
+def crear_curso(request):
+    return render(request, 'crearCurso.html')
+
+def asignar_notas(request):
+    return render(request, 'AsignarNotas.html')
