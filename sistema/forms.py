27a97dcbaaada172480a0d19110f16dcecb858bb
avillaq <villafuertequispealex@gmail.com>
2024-05-29 22:24:02 -0500

    Creamos la funcion save para el formulario de alumno

--- sistema/forms.py
+++ sistema/forms.py
@@ -23,3 +23,14 @@ class AlumnoForm(forms.ModelForm):
     class Meta:
         model = Alumno
         fields = ['nombre_alumno', 'apellido_alumno', 'cursos']
+
+    def save(self, commit=True):
+        nombre_alumno = self.cleaned_data.get('nombre_alumno')
+        apellido_alumno = self.cleaned_data.get('apellido_alumno')
+        cursos = self.cleaned_data.get('cursos')
+        alumno, estado = Alumno.objects.get_or_create(nombre_alumno=nombre_alumno, apellido_alumno=apellido_alumno)
+        for curso in cursos:
+            NotasAlumnosPorCurso.objects.get_or_create(id_alumno=alumno, id_curso=curso)
+        if commit:
+            alumno.save()
+        return alumno
