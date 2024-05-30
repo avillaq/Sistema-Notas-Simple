e2d749e54b74a0c339c79baaac71154f65689ad1
avillaq <villafuertequispealex@gmail.com>
2024-05-29 22:16:24 -0500

    Añadimos un nuevo campo en el formulario del alumno para los cursos en los que estara matriculado

--- sistema/forms.py
+++ sistema/forms.py
@@ -1,7 +1,7 @@
 from django import forms 
 from .models import Alumno, Curso, NotasAlumnosPorCurso
 # creating a form 
-class CursoForm(forms.Form): 
+class CursoForm(forms.ModelForm): 
     nombre_curso = forms.CharField(max_length=30, label='Nombre del curso') 
 
     class Meta:
@@ -15,10 +15,11 @@ class CursoForm(forms.Form):
             curso.save()
         return curso
 
-class AlumnoForm(forms.Form):
+class AlumnoForm(forms.ModelForm):
     nombre_alumno = forms.CharField(max_length=30, label='Nombre del alumno')
     apellido_alumno = forms.CharField(max_length=30, label='Apellidos del alumno')
+    cursos = forms.ModelMultipleChoiceField(queryset=Curso.objects.all(), label='Cursos')
 
     class Meta:
         model = Alumno
-        fields = ['nombre_alumno', 'apellido_alumno']
+        fields = ['nombre_alumno', 'apellido_alumno', 'cursos']
